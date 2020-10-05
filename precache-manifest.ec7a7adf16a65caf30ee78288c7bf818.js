39d22da1ac8ece4af429fea26754476f5bc5422e
yoon-junseo <ysj6844@naver.com>
2020-10-05 15:32:09 +0900

    Updates

---
+++ precache-manifest.ec7a7adf16a65caf30ee78288c7bf818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dea8939120af112e4707e8d124b470c",
+    "url": "/junseo_movie_2020/index.html"
+  },
+  {
+    "revision": "4fb5d58a8d635b73cca6",
+    "url": "/junseo_movie_2020/static/css/main.578e030e.chunk.css"
+  },
+  {
+    "revision": "a2099b6ef3c712d6e939",
+    "url": "/junseo_movie_2020/static/js/2.e6039046.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/junseo_movie_2020/static/js/2.e6039046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb5d58a8d635b73cca6",
+    "url": "/junseo_movie_2020/static/js/main.213d1862.chunk.js"
+  },
+  {
+    "revision": "4acd0736372c19826557",
+    "url": "/junseo_movie_2020/static/js/runtime-main.3444a8b2.js"
+  }
+]);
