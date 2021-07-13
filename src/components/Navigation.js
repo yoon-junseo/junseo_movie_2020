d0e6863caa249141579a284e46c63914a071a4b9
junseo <ysj6844@naver.com>
2021-07-13 21:31:10 +0900

    modify

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,8 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <div className="nav">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/">HOME</Link>
+      <Link to="/about">ABOUT</Link>
     </div>
   );
 }
